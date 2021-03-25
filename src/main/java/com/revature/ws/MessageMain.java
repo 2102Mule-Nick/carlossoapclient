d44e8895b1411a89a114e362fa5225de56d39f41
carlos <rudelli.quimson@yahoo.com>
2021-03-25 17:47:07 -0400

    working soap client

---
+++ src/main/java/com/revature/ws/MessageMain.java
@@ -0,0 +1,14 @@
+package com.revature.ws;
+
+public class MessageMain {
+	
+	public static void main(String[] args) {
+		
+		MessageGenService msgen = new MessageGenService();
+		
+		MessageGen msport = msgen.getMessageGenImplPort();
+		
+		System.out.println(msport.getMessage());
+	}
+	
+}
